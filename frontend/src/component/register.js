3959e665123c58f5c3efea2f628e5e46235b3496
SamuelSigalingging <samuelsigalingging566@gmail.com>
2026-07-16 22:14:10 +0700

    feat:coba-coba branch laman register

---
+++ frontend/src/component/register.js
@@ -0,0 +1,12 @@
+import react, { useState } from "react";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    nik: "",
+    email: "",
+    password: "",
+    full_name: "",
+    address: "",
+    phone: "",
+  });
+}
